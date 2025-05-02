bf56102274f196db878ea51852379d98f093d63a
tri <triledacminh@gmail.com>
2025-05-03 02:09:16 +0700

    update

--- custom/reid_infer.py
+++ custom/reid_infer.py
@@ -6,7 +6,7 @@ import mmcv
 from tqdm import tqdm
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'fast-reid')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'fast-reid')))
 print("\n".join(sys.path))
 
 class reid_inferencer():
