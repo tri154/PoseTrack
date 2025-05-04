c37d7a060f533fec79dd1aad5d3620f5f66bfbeb
tri <triledacminh@gmail.com>
2025-05-04 22:34:17 +0700

    update

--- custom/final_result.py
+++ custom/final_result.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os.path as osp
-
-file = np.loadtxt(osp.join('..','custom_result', 'track_results.txt'))[:,:-1]
+import os
+file = np.loadtxt(osp.join(os.getcwd(), '..','custom_result', 'track_results.txt'))[:,:-1]
 result = np.array(file)
 np.savetxt(osp.join('..','custom_result', 'track.txt'), result, fmt="%d %d %d %d %d %d %d %f %f")
