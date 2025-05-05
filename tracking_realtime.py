933cae33b8cc247621b82cdc10c794ab8256e057
tri <triledacminh@gmail.com>
2025-05-05 13:34:13 +0700

    update

--- tracking_realtime.py
+++ tracking_realtime.py
@@ -3,6 +3,10 @@ from worker import process_video
 import os
 from custom.prepare_model import prepare_model
 from custom.tracking import get_pose_tracker
+import numpy as np
+VIDEO_1="/kaggle/input/aic2024-sample/cam1-537/537_shorten.mp4"
+VIDEO_2="/kaggle/input/aic2024-sample/cam2-543/543_shorten.mp4"
+SAVE_PATH="/kaggle/working/PoseTrack/custom_result/track_results.txt"
 def main():
 
     engine_file = "yolo11l.engine"
@@ -14,8 +18,8 @@ def main():
     q0 = Queue()
     q1 = Queue()
 
-    p0 = Process(target=process_video, args=(0, "video1.mp4", 0, q0))
-    p1 = Process(target=process_video, args=(1, "video2.mp4", 1, q1))
+    p0 = Process(target=process_video, args=(0, VIDEO_1, 0, q0))
+    p1 = Process(target=process_video, args=(1, VIDEO_2, 1, q1))
 
     p0.start()
     p1.start()
@@ -31,9 +35,12 @@ def main():
             pose_tracker.mv_update_wo_pred(detection_sample_mv, frame_id)
             frame_results = pose_tracker.output(frame_id)
             results += frame_results
-
     p0.join()
     p1.join()
+    results = np.concatenate(results,axis=0)
+    sort_idx = np.lexsort((results[:,2],results[:,0]))
+    results = np.ascontiguousarray(results[sort_idx])
+    np.savetxt(SAVE_PATH, results)
 
 if __name__ == "__main__":
     main()
