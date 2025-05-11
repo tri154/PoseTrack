5f980a0185a7ec54463a013d3ac99ee9e3734420
tri <triledacminh@gmail.com>
2025-05-11 07:58:55 +0700

    update

--- tracking_realtime.py
+++ tracking_realtime.py
@@ -10,7 +10,7 @@ from kafka import KafkaProducer
 import json
 
 producer = KafkaProducer(
-    bootstrap_servers='42.118.0.103:8557',
+    bootstrap_servers='42.118.0.103:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
